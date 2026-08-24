d514646b854d219afad6d5a709a7569a90b0ea9f
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-08-24 12:32:17 -0500

    Ejercicio 01 Semana 4 completado - (Strategy + Factory method)

--- src/main/dosw/semana_4/taller4/Ejercicio01_PlataformaPagosInteligentes/PseStrategy.java
+++ src/main/dosw/semana_4/taller4/Ejercicio01_PlataformaPagosInteligentes/PseStrategy.java
@@ -1,5 +1,8 @@
 package main.dosw.semana_4.taller4.Ejercicio01_PlataformaPagosInteligentes;
 
-public class PseStrategy {
-    
+public class PseStrategy implements PaymentStrategy{
+    @Override
+    public void process(double amount) {
+        System.out.println("Procesando pago con PSE por $" + amount);
+    }
 }
--- src/main/dosw/semana_4/taller4/Ejercicio01_PlataformaPagosInteligentes/TarjetaStrategy.java
+++ src/main/dosw/semana_4/taller4/Ejercicio01_PlataformaPagosInteligentes/TarjetaStrategy.java
@@ -1,5 +1,8 @@
 package main.dosw.semana_4.taller4.Ejercicio01_PlataformaPagosInteligentes;
 
-public class TarjetaStrategy {
-    
+public class TarjetaStrategy implements PaymentStrategy{
+    @Override
+    public void process(double amount){
+        System.out.println("Procesando pago con tarjeta por $" + amount);
+    }
 }
